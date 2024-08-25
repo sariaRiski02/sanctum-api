16823ae118c1a0884561764cef5f8af76c484ce9
sariaRiski02 <muhammatsaria026@student.unsrat.ac.id>
2024-08-25 14:49:35 +0800

    modify controller

--- routes/api.php
+++ routes/api.php
@@ -11,6 +11,6 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::apiResource('posts', PostController::class);
-Route::post('/register', [AuthController::class, 'register'])->middleware('auth:sanctum');
+Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
